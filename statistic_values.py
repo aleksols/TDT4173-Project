f7a9bc620d57477fdb903385b095fee992ea9af9
Aleksander Scherman Olsen <aleko@stud.ntnu.no>
2020-11-21 03:16:13 +0100

    Print empty lines for clarity in statistic_values.py

--- statistic_values.py
+++ statistic_values.py
@@ -7,13 +7,18 @@ for column in df.columns:
     print(f"Mean for {column}:", df[column].mean())
     print(f"Std for {column}:", df[column].std())
 
+print()
+
 d, _, _ = utils.standardize_data(df)
 
 for column in [c for c in d.columns if "standardized" in c]:
     for col2 in [c for c in d.columns if "standardized" in c]:
         print(f"Correlation between {column} and {col2}: {d[column].corr(d[col2])}")
 
+print()
+
 d["Change"] = (d["Close"] / d["Close"].shift(1) - 1).fillna(0)
 
+print()
 print("Mean absolute daily change in closing price:", d["Change"].abs().mean())
 print("Std for absolute daily change in closing price:", d["Change"].abs().std())
