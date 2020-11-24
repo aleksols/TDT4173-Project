9858e12db12af68f4948c048c149a2f41437af8a
Aleksander Scherman Olsen <aleko@stud.ntnu.no>
2020-11-25 00:01:26 +0100

    Print mean and std for train, val and test indivitually

--- statistic_values.py
+++ statistic_values.py
@@ -1,12 +1,23 @@
 import pandas as pd
 
+import config
 import utils
 
 df = pd.read_pickle("timeseries_data/daily_data.pkl")
+lstm_train = df[:int(len(df) * config.train_partition)]
+lstm_val = df[int(len(df) * config.train_partition):-int(len(df) * config.test_partition)]
+lstm_test = df[-int(len(df) * config.test_partition):]
 
 for column in df.columns:
-    print(f"Mean for {column}:", df[column].mean())
-    print(f"Std for {column}:", df[column].std())
+    print(f"Mean for {column} in entire dataset:", df[column].mean())
+    print(f"Std for {column} in entire dataset:", df[column].std())
+    print(f"Mean for {column} in train dataset:", lstm_train[column].mean())
+    print(f"Std for {column} in train dataset:", lstm_train[column].std())
+    print(f"Mean for {column} in validation dataset:", lstm_val[column].mean())
+    print(f"Std for {column} in validation dataset:", lstm_val[column].std())
+    print(f"Mean for {column} in test dataset:", lstm_test[column].mean())
+    print(f"Std for {column} in test dataset:", lstm_test[column].std())
+    print()
 
 print()
 
